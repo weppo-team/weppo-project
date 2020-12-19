344fd3ee193a3359a0b58c749e416ab7daf87f81
Dawid Motak <dawidmotak1@gmail.com>
2020-12-20 00:24:35 +0100

    setup eslint and prettier for backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // serve the react app files
-app.use(express.static(`${__dirname}/frontend/build`));
+app.use(express.static(`${__dirname}/../frontend/build`));
 
 app.get('/api/hello', (_, res) => {
   res.json({ message: 'Hello world' });
