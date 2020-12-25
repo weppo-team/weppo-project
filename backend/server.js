67bd284e7f6b0b01d1c1207def506b8219d9b560
krystian <nlins8224@gmail.com>
2020-12-25 16:38:57 +0100

    Run prettier

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const app = express();
 
-import findConfig from 'find-config'
+import findConfig from 'find-config';
 import dotenv from 'dotenv';
-dotenv.config(({ path: findConfig('.env') }));
+dotenv.config({ path: findConfig('.env') });
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -11,7 +11,7 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import connection from '../backend/database/connection.js'
+import connection from '../backend/database/connection.js';
 connection().catch(console.error);
 
 // serve the react app files
