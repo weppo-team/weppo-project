d1e9e84d1fd97152f7e4bba3cbbbad9c34f29d25
krystian <nlins8224@gmail.com>
2020-12-25 16:25:12 +0100

    Add database client connection

---
+++ backend/database/connection.js
@@ -0,0 +1,38 @@
+import mongo from 'mongodb'
+import findConfig from 'find-config'
+import dotenv from 'dotenv';
+
+dotenv.config(({ path: findConfig('.env') }));
+const MongoClient = mongo.MongoClient;
+
+const uri = process.env.DB_URI
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+/*
+* listDatabases is purely for connection testing.
+* Expected output:
+* Databases:
+* - admin
+* - local
+* */
+
+const listDatabases = async (client) => {
+    const databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+const connection = async () => {
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+
+}
+
+export default connection
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 const app = express();
 
+import findConfig from 'find-config'
+import dotenv from 'dotenv';
+dotenv.config(({ path: findConfig('.env') }));
+
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+import connection from '../backend/database/connection.js'
+connection().catch(console.error);
 
 // serve the react app files
 app.use(express.static(`${__dirname}/../frontend/build`));
