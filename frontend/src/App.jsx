797f7888da9ac3f246830ab48a08fc31ca719e66
Dawid Motak <dawidmotak1@gmail.com>
2020-12-26 18:26:08 +0100

    added jsx extension to formatter and linter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,5 @@
-import { ThemeProvider } from 'styled-components'
-import { theme } from './style/theme'
-import { Routing } from './Routing'
-import { AppInfoProvider } from './context/AppInfoContext'
-
 export const App = () => (
-  <ThemeProvider theme={theme}>
-    <AppInfoProvider>
-      <Routing />
-    </AppInfoProvider>
-  </ThemeProvider>
+  <div>
+    <h1>Hello world</h1>
+  </div>
 )
