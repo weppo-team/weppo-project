baa85ab46ffada64d11082dbd4bc1f41e172e1d1
krystian <nlins8224@gmail.com>
2021-01-10 20:00:15 +0100

    Add user and game schemas

---
+++ backend/database/gameSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const gameSchema = new Schema({
+    elo: Number,
+    win: Number,
+    lose: Number,
+})
+
+module.exports = gameSchema;
