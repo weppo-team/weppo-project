0ec6007a057f81806b7b77f9794eea1717b213bf
Dawid Motak <dawidmotak1@gmail.com>
2021-01-31 18:33:35 +0100

    made game title clickable

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,10 @@
-import { Typography } from 'antd'
+import { Link } from 'react-router-dom'
 import { NavigationMenu } from './components/NavigationMenu'
 import { LoginMenu } from './components/LoginMenu'
 import { SidebarElements } from './elements'
 import { useAppInfo } from '../../context/AppInfoContext'
 
-const { Title } = Typography
-
-const { LogoWrapper, StyledSider, StyledDivider } = SidebarElements
+const { LogoWrapper, StyledSider, StyledDivider, StyledTitle } = SidebarElements
 
 export const Sidebar = () => {
   const { title } = useAppInfo()
@@ -14,7 +12,9 @@ export const Sidebar = () => {
   return (
     <StyledSider breakpoint="lg" collapsedWidth="0">
       <LogoWrapper>
-        <Title level={4}>{title}</Title>
+        <StyledTitle level={4}>
+          <Link to="/">{title}</Link>
+        </StyledTitle>
         <StyledDivider />
       </LogoWrapper>
       <NavigationMenu />
