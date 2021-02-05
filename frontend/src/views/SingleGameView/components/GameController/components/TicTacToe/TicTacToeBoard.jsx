88f7b8815a0d371773c7040df0b011426056ab68
Dawid Motak <dawidmotak1@gmail.com>
2021-02-05 12:13:15 +0100

    fixed multiple rerenders bug

--- frontend/src/views/SingleGameView/components/GameController/components/TicTacToe/TicTacToeBoard.jsx
+++ frontend/src/views/SingleGameView/components/GameController/components/TicTacToe/TicTacToeBoard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { Card, Avatar, message } from 'antd'
 import ticTacToeLogo from '../../../../../../logos/tic-tac-toe.svg'
 import { TicTacToeBoardElements } from './elements'
@@ -11,7 +12,7 @@ const { Meta } = Card
 const { Container, StyledCard, Grid } = TicTacToeBoardElements
 
 export const TicTacToeBoard = ({ socket, userData }) => {
-  const [roomName, setRoomName] = useState(null)
+  const { roomName } = useParams()
   const [playerSymbol, setPlayerSymbol] = useState(' ')
   const playerName =
     userData.usertype === 'guest'
@@ -20,17 +21,7 @@ export const TicTacToeBoard = ({ socket, userData }) => {
   const [opponentName, setOpponentName] = useState('[waiting for opponent]')
   const [opponentType, setOpponentType] = useState('N/A')
   const [canMove, setCanMove] = useState('false')
-  const [board, setBoardState] = useState([
-    ' ',
-    ' ',
-    ' ',
-    ' ',
-    ' ',
-    ' ',
-    ' ',
-    ' ',
-    ' ',
-  ])
+  const [board, setBoardState] = useState(Array(9).fill(' '))
 
   const setBoardTile = (i) => {
     console.log(`${canMove} - ${roomName}`)
@@ -44,51 +35,50 @@ export const TicTacToeBoard = ({ socket, userData }) => {
     }
   }
 
-  socket.on('giveUserdata', (data) => {
-    setRoomName(data.roomName)
-    socket.emit('giveUserdata', {
-      roomName: data.roomName,
-      username: userData.username,
-      usertype: userData.usertype,
+  useEffect(() => {
+    socket.on('giveUserdata', (data) => {
+      socket.emit('giveUserdata', {
+        roomName: data.roomName,
+        username: userData.username,
+        usertype: userData.usertype,
+      })
     })
-  })
 
-  socket.on('takeUserdata', (data) => {
-    if (data.username !== userData.username) {
-      setOpponentType(data.usertype)
-      setOpponentName(
-        opponentType === 'guest' ? `${data.username} (guest)` : data.username,
+    socket.on('takeUserdata', (data) => {
+      if (data.username !== userData.username) {
+        setOpponentType(data.usertype)
+        setOpponentName(
+          opponentType === 'guest' ? `${data.username} (guest)` : data.username,
+        )
+      }
+    })
+
+    socket.on('takeSymbol', (data) => {
+      setPlayerSymbol(data.playerSymbol)
+      if (playerSymbol === 'X') setCanMove(true)
+    })
+
+    socket.on('madeMove', (data) => {
+      setBoardState(data.newBoard)
+      console.log(board)
+      setCanMove(data.moveSymbol !== playerSymbol)
+      console.log(canMove)
+    })
+
+    socket.on('end-win', (data) => {
+      if (data.winnerSymbol === playerSymbol)
+        message.info('You won this round, next will start in 10 seconds', 10)
+      else
+        message.info('You lost this round, next will start in 10 seconds', 10)
+    })
+
+    socket.on('end-draw', () => {
+      message.info(
+        'You have reached draw, next round will start in 10 seconds',
+        10,
       )
-    }
-  })
-
-  socket.on('takeSymbol', (data) => {
-    console.log(`Got symbol: ${data.playerSymbol}`)
-    setPlayerSymbol(data.playerSymbol)
-    if (playerSymbol === 'X') setCanMove(true)
-  })
-
-  socket.on('madeMove', (data) => {
-    setBoardState(data.newBoard)
-    console.log(board)
-    setCanMove(data.moveSymbol !== playerSymbol)
-    console.log(canMove)
-  })
-
-  socket.on('end-win', (data) => {
-    console.log('WIN')
-    if (data.winnerSymbol === playerSymbol)
-      message.info('You won this round, next will start in 10 seconds', 10)
-    else message.info('You lost this round, next will start in 10 seconds', 10)
-  })
-
-  socket.on('end-draw', () => {
-    console.log('DRAW')
-    message.info(
-      'You have reached draw, next round will start in 10 seconds',
-      10,
-    )
-  })
+    })
+  }, [userData])
 
   return (
     <Container>
@@ -100,51 +90,14 @@ export const TicTacToeBoard = ({ socket, userData }) => {
         <PlayersLabel playerOne={playerName} playerTwo={opponentName} />
         <TurnLabel player="HomeTeam" playerSymbol={playerSymbol} />
         <Grid>
-          <Square
-            number={0}
-            setBoardTile={setBoardTile}
-            squareContent={board[0]}
-          />
-          <Square
-            number={1}
-            setBoardTile={setBoardTile}
-            squareContent={board[1]}
-          />
-          <Square
-            number={2}
-            setBoardTile={setBoardTile}
-            squareContent={board[2]}
-          />
-          <Square
-            number={3}
-            setBoardTile={setBoardTile}
-            squareContent={board[3]}
-          />
-          <Square
-            number={4}
-            setBoardTile={setBoardTile}
-            squareContent={board[4]}
-          />
-          <Square
-            number={5}
-            setBoardTile={setBoardTile}
-            squareContent={board[5]}
-          />
-          <Square
-            number={6}
-            setBoardTile={setBoardTile}
-            squareContent={board[6]}
-          />
-          <Square
-            number={7}
-            setBoardTile={setBoardTile}
-            squareContent={board[7]}
-          />
-          <Square
-            number={8}
-            setBoardTile={setBoardTile}
-            squareContent={board[8]}
-          />
+          {[...Array(9)].map((_, index) => (
+            <Square
+              key={`square-${index + 1}`}
+              number={index}
+              setBoardTile={(arg) => setBoardTile(arg)}
+              squareContent={board[index]}
+            />
+          ))}
         </Grid>
       </StyledCard>
     </Container>
