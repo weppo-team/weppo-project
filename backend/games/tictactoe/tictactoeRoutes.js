c600081beae7ea42c33ebd5a46fe287998352973
krystian <nlins8224@gmail.com>
2021-02-08 16:30:01 +0100

    Fix checkIfWon

--- backend/games/tictactoe/tictactoeRoutes.js
+++ backend/games/tictactoe/tictactoeRoutes.js
@@ -5,14 +5,14 @@ const ROOM_SIZE_LIMIT = 2;
 const checkIfDraw = (board) => !board.includes(' ')
 
 const checkIfWon = (board) => 
-                    (board[0] !== ' ' && board[0] === board[1] === board[2]) ||
-                    (board[3] !== ' ' && board[3] === board[4] === board[5]) ||
-                    (board[6] !== ' ' && board[6] === board[7] === board[8]) ||
-                    (board[0] !== ' ' && board[0] === board[3] === board[6]) ||
-                    (board[1] !== ' ' && board[1] === board[4] === board[7]) ||
-                    (board[2] !== ' ' && board[2] === board[5] === board[8]) ||
-                    (board[0] !== ' ' && board[0] === board[4] === board[8]) ||
-                    (board[2] !== ' ' && board[2] === board[4] === board[6])
+                    (board[0] !== ' ' && board[0] === board[1] && board[1] === board[2]) ||
+                    (board[3] !== ' ' && board[3] === board[4] && board[4] === board[5]) ||
+                    (board[6] !== ' ' && board[6] === board[7] && board[7] === board[8]) ||
+                    (board[0] !== ' ' && board[0] === board[3] && board[3] === board[6]) ||
+                    (board[1] !== ' ' && board[1] === board[4] && board[4] === board[7]) ||
+                    (board[2] !== ' ' && board[2] === board[5] && board[5] === board[8]) ||
+                    (board[0] !== ' ' && board[0] === board[4] && board[4] === board[8]) ||
+                    (board[2] !== ' ' && board[2] === board[4] && board[4] === board[6])
 
 module.exports = function (app, io) {
   const tictactoeRooms = io.of('/api/sockets/tictactoe');
